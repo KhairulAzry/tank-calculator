c99625a0843bca7240dcd23a179a8b1e891bef89
Khairul Azry <mkhairulazry@gmail.com>
2020-03-26 20:46:55 +0800

    2nd design
    show by percentage with 3 color mode

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
-function calculateTankSize(argument) {
-	// body...
-}
+var fluidpercent = fluidamount / tankvolume * 100 ;
+
+var tankvolume = 3.14159265 * tankdiameter * tanklength;
+
+var fluidamount = liquidheight;
